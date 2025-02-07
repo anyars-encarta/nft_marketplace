b6668384e31cbe06d40aecf381d75e0299ade1c7
Anyars Yussif <anyarsencarta@gmail.com>
2025-02-07 14:09:22 +0000

    Install and configure eslint

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
-export default function Home() {
+const  Home = () => {
   return (
     <div>
       <h1 className='text-3xl font-bold underline capitalize'>Hello, World!</h1>
     </div>
   );
 }
+
+export default Home;
