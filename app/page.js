21ae218d0bdacbfa44174bbf7e7b1cec0abd4f54
Anyars Yussif <anyarsencarta@gmail.com>
2025-02-07 16:17:36 +0000

    implement basic setup and routes

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
 const  Home = () => {
   return (
     <div>
-      <h1 className='text-3xl font-bold underline capitalize'>Hello, World!</h1>
+      <h1 className='text-3xl font-bold underline capitalize flexCenter'>
+        Hello, World!
+      </h1>
     </div>
   );
 }
