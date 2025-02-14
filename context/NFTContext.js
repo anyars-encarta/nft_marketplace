8898982a46e780b7a94a986fec8d5442f10a66b6
Anyars Yussif <anyarsencarta@gmail.com>
2025-02-14 12:30:44 +0000

    Change ipfs-http-client to use helia

--- context/NFTContext.js
+++ context/NFTContext.js
@@ -1,50 +1,79 @@
-'use client';
+"use client";
 
-import React, { useState, useEffect } from 'react';
-import Web3Modal from 'web3modal';
-import { ethers } from 'ethers';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import Web3Modal from "web3modal";
+import { ethers } from "ethers";
+import axios from "axios";
+import { createHelia } from "helia";
+import { unixfs } from "@helia/unixfs";
+import { multiaddr } from "@multiformats/multiaddr";
 
-import {MarketAddress, MarketAddressABI} from '../constants/constants';
+import { MarketAddress, MarketAddressABI } from "../constants/constants";
 
 export const NFTContext = React.createContext();
 
 export const NFTProvider = ({ children }) => {
-    const [currentAccount, setCurrentAccount] = useState('')
-    const nftCurrency = 'MATIC';
+  const [currentAccount, setCurrentAccount] = useState("");
+  const nftCurrency = "MATIC";
 
-    const checkIfWalletIsConnected = async () => {
-        if(!window.ethereum) return alert("Please install MetaMask");
+  const checkIfWalletIsConnected = async () => {
+    if (!window.ethereum) return alert("Please install MetaMask");
 
-        const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+    const accounts = await window.ethereum.request({ method: "eth_accounts" });
 
-        if(accounts.length) {
-            setCurrentAccount(accounts[0]);
-        } else {
-            alert("No account found");
-        }
-    };
+    if (accounts.length) {
+      setCurrentAccount(accounts[0]);
+    } else {
+      alert("No account found");
+    }
+  };
 
-    useEffect(() => {
-        checkIfWalletIsConnected();
-    }, []);
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
 
+  const connectWallet = async () => {
+    if (!window.ethereum) return alert("Please install MetaMask");
 
-    const connectWallet = async () => {
-        if(!window.ethereum) return alert("Please install MetaMask");
+    if (!window.ethereum) return alert("Please install MetaMask");
 
-        if(!window.ethereum) return alert("Please install MetaMask");
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
 
-        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    setCurrentAccount(accounts[0]);
 
-        setCurrentAccount(accounts[0]);
+    window.location.reload();
+  };
 
-        window.location.reload();
-    };
+  const uploadToIPFS = async (file) => {
+    const helia = await createHelia({
+      addresses: {
+        api: multiaddr("/dns4/ipfs.infura.io/tcp/5001/https"),
+      },
+    });
 
-    return (
-        <NFTContext.Provider value={{ nftCurrency, connectWallet, currentAccount }}>
-            {children}
-        </NFTContext.Provider>
-    )
-}
+    const fs = unixfs(helia);
+
+    try {
+      const arrayBuffer = await file.arrayBuffer();
+      const buffer = new Uint8Array(arrayBuffer);
+
+      const cid = await fs.addBytes(buffer);
+
+      const url = `https://ipfs.infura.io/ipfs/${cid.toString()}`;
+
+      return url;
+    } catch (e) {
+      console.error("Error uploading file to IPFS: ", e);
+    }
+  };
+
+  return (
+    <NFTContext.Provider
+      value={{ nftCurrency, connectWallet, currentAccount, uploadToIPFS }}
+    >
+      {children}
+    </NFTContext.Provider>
+  );
+};
