a7e328a75e82a9179b31db82eba761d7ecd8df66
Anyars Yussif <anyarsencarta@gmail.com>
2025-02-15 12:03:56 +0000

    Implement fetch NFTS

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        domains: ["ipfs.infura.io"],
+    },
+};
 
 export default nextConfig;
